bea5e388fe799ab19138828348221df44a5defa3
Katherine Manning <katmann1224@gmail.com>
2020-08-12 17:17:56 -0400

    fixed code for tobs and prcp queries

--- app.py
+++ app.py
@@ -63,27 +63,27 @@ def prcp():
 #     # Create our session (link) from Python to the DB
     session = Session(engine)
     # Find last point in data to calculate 1 year ago
-    lastpoint = session.query(measurement.date).order_by(measurement.date.desc()).first().date
+    lastpoint = session.query(measurement.date).order_by(measurement.date.desc()).first()[0]
 
     lastpoint=dt.datetime.strptime(lastpoint, "%Y-%m-%d")
 
 # Calculate the date 1 year ago from the last data point in the database
     year_ago = dt.date(2017,8,23) - relativedelta(months=12)
 
-    query = session.query(measurement.date, measurement.prcp).filter(measurement.date >= year_ago).all()
-   
-    
-    #I tried doing a dict but it was only returing one date.
+    query = session.query(measurement.date,measurement.prcp).\
+        filter(measurement.date >= year_ago).all()
+
+  
+    #dictionary
     fullprcplist = []
     for date, prcp in query:
         prcp_dict = {}
-        prcp_dict["Precipitation"] = prcp
         prcp_dict["Date"] = date
+        prcp_dict["Precipitation"] = prcp
         fullprcplist.append(prcp_dict)
+        
 
-    #using the list function returned all prcp values for the past year
-        prcplist2 = list(np.ravel(query))
-        return jsonify(prcplist2)
+    return jsonify(fullprcplist)
      
     
 
@@ -93,9 +93,9 @@ def stations():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    # Query percipitation
-    stations = session.query(station.station).all()
-
+    # Query stations
+    stations = session.query(station.station).order_by(station.station).all()
+    stations = list(np.ravel(stations))
 
     return jsonify(stations)
 
@@ -104,34 +104,43 @@ def stations():
 def tobs():
     # Create our session (link) from Python to the DB
     session = Session(engine)
+    # Calculate the date 1 year ago from the last data point in the database
     lastpoint = session.query(measurement.date).order_by(measurement.date.desc()).first().date
     lastpoint
     lastpoint=dt.datetime.strptime(lastpoint, "%Y-%m-%d")
     year_ago = dt.date(2017,8,23) - relativedelta(months=12)
-    # Calculate the date 1 year ago from the last data point in the database
-    year_ago = dt.date(2017,8,23) - relativedelta(months=12)
-    # Query percipitation
-    topstations = session.query(measurement.station,func.count(measurement.station)).group_by(measurement.station).order_by(func.count(measurement.station).desc()).all()
+
+    # Query topstations
+    topstations = session.query(measurement.station,func.count(measurement.station)).group_by(measurement.station).order_by(func.count(measurement.station).desc()).\
+        filter(measurement.date >= '2016-08-23').all()
     #Under the assumption I need to use the highest row count from query
     topstation1 = (topstations[0])
     topstation1 = (topstation1[0])
 
 # Using the station id from the previous query, calculate the lowest temperature recorded, 
 # highest temperature recorded, and average temperature of the most active station?
-    session.query(func.min(measurement.tobs),func.avg(measurement.tobs),func.max(measurement.tobs),func.count(measurement.tobs)).filter(measurement.station == topstation1).all()
-    top_station = session.query(measurement.tobs).\
-    filter(measurement.station == topstation1).filter(measurement.date >= year_ago).all()
-    return jsonify(top_station)
+    query = session.query((measurement.date),func.min(measurement.tobs),func.avg(measurement.tobs),func.max(measurement.tobs)).\
+        filter(measurement.station == 'USC00519397').filter(measurement.date >= year_ago).group_by(measurement.date).all()
 
 
+    toplist = []
+    for date, min, avg, max in query:
+       loopyboi = {}
+       loopyboi["Date"] = date
+       loopyboi["Minimum Temp"] = min
+       loopyboi["Average Temp"] = avg
+       loopyboi["Max Temp"] = max
+       toplist.append(loopyboi)
 
+       
+    return jsonify(toplist)
 
 @app.route('/api/v1.0/temp/<start>', defaults={'end': None})
-
 def temperatures(start, end):
+    # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    
+       # Query
     query = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
         filter(measurement.date >= start).all()
 
